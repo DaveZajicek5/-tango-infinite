b690c3f3153a83489cc17e7d0df9772790642085
DaveZajicek5 <77385685+DaveZajicek5@users.noreply.github.com>
2026-07-06 23:03:26 +0200

    Load hint pattern patch through solver response

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'tango-infinite-v12';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './engine.js', './solver.js', './ui.js', './ui-next.js'];
+const CACHE = 'tango-infinite-v13';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './engine.js', './solver.js', './hint-plus.js', './ui.js', './ui-next.js'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
@@ -7,21 +7,36 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
+
+async function getText(pathOrRequest) {
+  const cached = await caches.match(pathOrRequest);
+  if (cached) return cached.text();
+  const response = await fetch(pathOrRequest);
+  const copy = response.clone();
+  caches.open(CACHE).then(cache => cache.put(pathOrRequest, copy));
+  return response.text();
+}
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   const url = new URL(event.request.url);
+
+  if (url.pathname.endsWith('/solver.js')) {
+    event.respondWith((async () => {
+      const solver = await getText(event.request);
+      const hints = await getText('./hint-plus.js');
+      return new Response(solver + '\n' + hints, { headers: { 'Content-Type': 'application/javascript; charset=utf-8' } });
+    })());
+    return;
+  }
+
   const request = url.pathname.endsWith('/ui.js') ? './ui-next.js' : event.request;
-  event.respondWith(
-    caches.match(request).then(hit => hit || fetch(request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
-  );
+  event.respondWith(caches.match(request).then(hit => hit || fetch(request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(request, copy));
+    return response;
+  }).catch(() => caches.match('./index.html'))));
 });
