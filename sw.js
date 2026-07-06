4d00290e19299ea7fa2cb27520418c37cc9f4a7c
DaveZajicek5 <77385685+DaveZajicek5@users.noreply.github.com>
2026-07-06 22:29:56 +0200

    Route app to polished UI build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'tango-infinite-v9';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './engine.js', './solver.js', './ui.js'];
+const CACHE = 'tango-infinite-v10';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './engine.js', './solver.js', './ui.js', './ui-next.js'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
@@ -15,10 +15,12 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  const request = url.pathname.endsWith('/ui.js') ? './ui-next.js' : event.request;
   event.respondWith(
-    caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    caches.match(request).then(hit => hit || fetch(request).then(response => {
       const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      caches.open(CACHE).then(cache => cache.put(request, copy));
       return response;
     }).catch(() => caches.match('./index.html')))
   );
